f7201b048cb1124a200a1ab724e5d94ed9bc510a
Eduardo Agusti <eduardoagusti@gmail.com>
2026-03-26 20:38:13 +0100

    Update2603

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
    <footer className="border-t py-10 text-sm text-gray-500">
   <div className="max-w-6xl mx-auto px-6">
-    © 2026 Eduardo Agustí — UX/UI Designer
+    © 2026 Eduardo Agustí — Product Designer &  more.
   </div>
 </footer>
   );
